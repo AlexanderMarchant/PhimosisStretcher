5ce1cc5b0f95bb8dfee34e4be1480d196b9b3b34
Alex Marchant <alex_marchnat@outlook.com>
2020-05-23 15:26:10 +0100

    Add website

---
+++ Website/phimosis-stretches/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrivacyPolicyComponent } from './privacy/privacy-policy/privacy-policy.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrivacyPolicyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
