0254eaeac3a98bef27252510c96cee64c85b5856
Alex Marchant <alex_marchnat@outlook.com>
2020-05-23 17:58:55 +0100

    Update website

--- Website/phimosis-stretches/src/app/app-routing.module.ts
+++ Website/phimosis-stretches/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
+import { PrivacyPolicyComponent} from './privacy-policy/privacy-policy.component'
+import { LandingPageComponent } from './landing-page/landing-page.component'
+
 
 const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent
-  }
+  { path: 'privacy', component: PrivacyPolicyComponent},
+  { path: '', component: LandingPageComponent}
 ];
 
 @NgModule({
