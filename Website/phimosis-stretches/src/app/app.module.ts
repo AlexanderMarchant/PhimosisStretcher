25fa1550e29190190f0d1642094eae8c1f799af9
Alex Marchant <alex_marchnat@outlook.com>
2020-05-23 15:27:06 +0100

    Update document

--- Website/phimosis-stretches/src/app/app.module.ts
+++ Website/phimosis-stretches/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrivacyPolicyComponent } from './privacy/privacy-policy/privacy-policy.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PrivacyPolicyComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
